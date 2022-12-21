9622fddd27d0672a556a95696051e83f5a7a5b5f
Ramidy <dumbhumanist@gmail.com>
2022-12-21 07:18:40 +0200

    SemenCommander Api initial commit

---
+++ SemenCommanderApi/SemenCommanderApi/Controllers/BackupController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SemenCommanderApi.Controllers
+{
+    [ApiController]
+    [Route("backup/{userId}")]
+    public class BackupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet("all")]
+        public ActionResult<string> GetAllFiles(Guid userId)
+        {
+            return "Some data";
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult<string> GetFile(Guid userId, Guid fileId)
+        {
+            return "Some data";
+        }
+
+        [HttpPost]
+        public IActionResult PostFile(Guid userId)
+        {
+            return Ok();
+        }
+
+    }
+}
